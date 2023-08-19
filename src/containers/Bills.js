4249c2e98a6a029f7a5c4800ccfa1cb73b33de27
mamadykonte <contactmamadykonte@gmail.com>
2023-08-20 01:53:17 +0200

    [Bug report] - Bills

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -34,24 +34,25 @@ export default class {
       .list()
       .then(snapshot => {
         const bills = snapshot
-          .map(doc => {
+          .sort((a, b) => new Date(b.date) - new Date(a.date))
+          .map((doc) => {
             try {
               return {
                 ...doc,
                 date: formatDate(doc.date),
-                status: formatStatus(doc.status)
-              }
-            } catch(e) {
+                status: formatStatus(doc.status),
+              };
+            } catch (e) {
               // if for some reason, corrupted data was introduced, we manage here failing formatDate function
               // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
+              console.log(e, "for", doc);
               return {
                 ...doc,
                 date: doc.date,
-                status: formatStatus(doc.status)
-              }
+                status: formatStatus(doc.status),
+              };
             }
-          })
+          });
           console.log('length', bills.length)
         return bills
       })
